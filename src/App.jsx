83c16ae51c26cbbd096277dda5213edb5d17f99b
Prince Gupta <129318983+Monkey-d-prince@users.noreply.github.com>
2025-09-01 17:35:36 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from './components/Dashboard/Dashboard';
+import './components/Dashboard/Dashboard.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
